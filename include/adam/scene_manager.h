b93f38085d73ae22c2c2074d3e95d720898f50d0
Enrico <enricomoras06@gmail.com>
2026-09-22 16:53:00 +0200

    chore: remove Settings feature

--- include/adam/scene_manager.h
+++ include/adam/scene_manager.h
@@ -1,7 +1,6 @@
 #ifndef SCENE_MANAGER_H
 #define SCENE_MANAGER_H
 
-#include "adam/game_settings.h"
 #include "adam/scene.h"
 #include <memory>
 
@@ -9,7 +8,6 @@ class SceneManager {
   private:
     std::unique_ptr<Scene> m_currentScene = nullptr;
     std::unique_ptr<Scene> m_pendingScene = nullptr;
-    std::unique_ptr<GameSettings> m_settings = std::make_unique<GameSettings>();
 
     void applyPendingScene();
 
@@ -17,10 +15,6 @@ class SceneManager {
     void changeScene(std::unique_ptr<Scene> newScene);
     void update(float deltaTime);
     void render();
-
-    const std::unique_ptr<GameSettings> &getSettings() const {
-        return this->m_settings;
-    }
 };
 
 #endif // SCENE_H
--- src/game_settings.cpp
+++
@@ -1,1 +0,0 @@
-#include "adam/game_settings.h"
